435787b717238be34d8bca18c1286447d270c464
mmendes1 <mmendes1@worcester.edu>
2020-02-15 13:25:52 -0500

    Version 2.0

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Todoroki'},
+    { id: 12, name: 'Deku'},
+    { id: 13, name: 'Bakugo'},
+    { id: 14, name: 'All Might'},
+    { id: 15, name: 'Lemillion'},
+    { id: 16, name: 'Fat Gum'},
+    { id: 17, name: 'Red Riot'},
+    { id: 18, name: 'Eraser Head'},
+    { id: 19, name: 'Endeavor'},
+    { id: 20, name: 'Hawks'}
+];
